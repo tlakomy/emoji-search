d43f7571ff3cafdd83531df950f957e97dfc5781
Tomasz Łakomy <tomasz.lakomy@olx.pl>
2020-03-18 17:55:22 +0100

    Added code for lesson 02

--- cypress/integration/basic.spec.js
+++ cypress/integration/basic.spec.js
@@ -1,5 +1,6 @@
 describe("my first e2e test", () => {
     it("actually works", () => {
-        expect(true).to.equal(true);
+        cy.visit("/");
+        cy.contains("Emoji Search");
     });
 });
