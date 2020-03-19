13b50bdd45eb83a2a50d9c595c33566b019aa09a
Tomasz Łakomy <tomasz.lakomy@olx.pl>
2020-03-19 20:19:02 +0100

    Added code for lesson 05

--- cypress/integration/basic.spec.js
+++ cypress/integration/basic.spec.js
@@ -1,13 +1,15 @@
 /// <reference types="Cypress" />
 
 describe("Emoji Search app", () => {
-    it("successfully renders the page header", () => {
+    beforeEach(() => {
         cy.visit("/");
+    });
+
+    it("successfully renders the page header", () => {
         cy.contains("Emoji Search");
     });
 
     it("renders the list of emojis", () => {
-        cy.visit("/");
         cy.get("[data-cy='emoji-row']").should("have.length", 20);
     });
 });
