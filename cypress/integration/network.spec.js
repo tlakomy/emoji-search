ac436209bfa5ebe00d92d9c0b756814f4eb8a06e
Tomasz Łakomy <tomasz.lakomy@olx.pl>
2020-03-23 20:03:43 +0100

    Added code for lesson 09

--- cypress/integration/network.spec.js
+++ cypress/integration/network.spec.js
@@ -1,8 +1,15 @@
 /// <reference types="Cypress" />
-import { ENDPOINT } from "./utils";
+import { ENDPOINT, EMOJI_RESPONSE } from "./utils";
 
 describe("Emoji Search app  - network tests", () => {
     it("displays emojis returned from the backend", () => {
+        cy.server();
+        cy.route("GET", ENDPOINT, EMOJI_RESPONSE);
+
         cy.visit("/");
+        cy.get("[data-cy='emoji-row']").contains("Joy");
+        cy.get("[data-cy='emoji-row']").contains("Smiley");
+        cy.get("[data-cy='emoji-row']").contains("Smile");
+        cy.get("[data-cy='emoji-row']").should("have.length", 3);
     });
 });
